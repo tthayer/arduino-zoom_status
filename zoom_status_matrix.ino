b512e346ea74f30daca13988e21a00b70669f118
Tony Thayer-Osborne <anthony.thayerosborne@skyward.io>
2021-03-24 17:03:55 -0700

    M5 Atom version w/ bluetooth keyboard functionality

--- zoom_status_matrix.ino
+++ zoom_status_matrix.ino
@@ -1,28 +1,42 @@
-#include <FastLED.h>
+#include "M5Atom.h"
+#include <BleKeyboard.h>
 
-#define NUM_LEDS 25
-#define DATA_PIN 27
+BleKeyboard bleKeyboard("M5AtomButton");
 
-int zoom_mute_status;
-
+int zoom_mute_status = 1;
 CRGB leds[NUM_LEDS];
+
 void setup() {
+  M5.begin(true, true, true);
+  bleKeyboard.begin();
   Serial.begin(9600);
-  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(18);
+  M5.dis.drawpix(0, 0xf00000);
 }
 
 void loop() {
+  delay(10);
+  M5.update();
+  if (bleKeyboard.isConnected()) {
+    if (M5.Btn.pressedFor(20)) {
+      bleKeyboard.press(KEY_LEFT_SHIFT);
+      delay(100);
+      bleKeyboard.press(KEY_LEFT_ALT);
+      delay(100);
+      bleKeyboard.press(KEY_LEFT_GUI);
+      delay(100);
+      bleKeyboard.press('m');
+      delay(100);
+      bleKeyboard.releaseAll();
+    }
+  }
   if (Serial.available() > 0) {
     String serial_input = Serial.readString();
     zoom_mute_status = serial_input.toInt();
-    Serial.print("prior zoom status: ");
-    Serial.println(zoom_mute_status);
-    Serial.println(zoom_mute_status);
     switch (zoom_mute_status) {
-      case 0: { fill_solid(leds, NUM_LEDS, CRGB::Blue); FastLED.show(); break; }
-      case 1: { fill_solid(leds, NUM_LEDS, CRGB::Red); FastLED.show(); break; }
-      case 2: { fill_solid(leds, NUM_LEDS, CRGB::Black); FastLED.show(); break; }
+      case 0: { M5.dis.drawpix(0, 0xFF0000); break; }
+      case 1: { M5.dis.drawpix(0, 0x00FF00); break; }
+      case 2: { M5.dis.drawpix(0, 0xf00000); break; }
     }
+    Serial.flush();
   }
 }
